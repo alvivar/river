bc9f5414313fdce77be58f9fdad4aed272537886
alvivar <andresalvivar@gmail.com>
2017-02-12 14:38:19 -0600

    Ready to start playing with Tweetinvi, the previous tweet library wasn't full compatible with mono

--- river/Program.fs
+++ river/Program.fs
@@ -1,11 +1,23 @@
 ﻿
+(*  River v0.1
+    Queue tweets by dropping images into a folder!
+
+    By twitter.com/matnesis
+
+
+    To do
+        x Learn to Yaml
+        - Learn to tweet images/text
+        - Scan current folder for images
+        - CRUD a Yaml based on images found
+        - Read from a Yaml with time to post
+        - Post once to twitter the images with a time *)
+
+
 open FSharp.Configuration
-open FSharp.Data.Toolbox.Twitter
-open System.Windows.Forms
 
 
-(* YAML Config *)
-
+(*  YAML Config *)
 [<Literal>] // This allows the bind at compile time
 let YamlFile = "/Users/andresv/Projects/river/river/config.yaml" // By using this provider I can read yaml
 
@@ -13,14 +25,12 @@ type ConfigFile = YamlConfig<YamlFile>
 let data = ConfigFile()
 data.Load(YamlFile) // Load is important, at defining the provider only the schema gets loaded
 
-
 // Sorting by date | F# Sequence to generic list
 data.waiting <- new System.Collections.Generic.List<ConfigFile.waiting_Item_Type>(data.waiting |> Seq.sortBy (fun x -> x.date))
 data.Save(YamlFile)
 
 
-(* Twitter *)
-
+(*  Twitter *)
 type TweetData = {
     message : string;
     image: string;
@@ -31,20 +41,10 @@ let key = "w9HTFA5cCDPhmLqUYqrSK6kXK"
 let secret = "kkrn0e66g5hpKzLwEbnvY6KVR4pqUfDnJNBvoXcgjqCvZGu1Bv"
 let token = "55159989-NThkem3iDw1U1L3fEA0iLdwqYfeEkPHzLRfNPg4uI"
 let tokenSecret = "DboMLBHwRBVW2QQmpEXPBbs1UEMIeFkHNvEgxJT4q8Z8Z"
-let twitcon = Twitter.AuthenticateAppSingleUser(key, secret, token, tokenSecret)
-
-let tweet = twitcon.Tweets.Post("This tweet is a test")
 
 
-(* MAIN *)
-
+(*  MAIN *)
 [<EntryPoint>]
 let main argv =
     printfn "%A" argv
     0 // Exit code
-
-
-// TODO
-// - Read config files
-// - Post to twitter image / text
-// - Scans images
