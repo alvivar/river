aa16a84b1a4bb293fb59de488758637d4f85e276
alvivar <andresalvivar@gmail.com>
2017-02-12 18:43:31 -0600

    Tweetinvi doesn't work, and this breaks my heart

--- river/Program.fs
+++ river/Program.fs
@@ -10,14 +10,17 @@
         - Learn to tweet images/text
         - Scan current folder for images
         - CRUD a Yaml based on images found
-        - Read from a Yaml the times to post
+        - Read from a Yaml with time to post
         - Post once to twitter the images with a time *)
 
 
 open FSharp.Configuration
+open Tweetinvi
+open System.IO
 
 
 (*  YAML Config *)
+
 [<Literal>] // This allows the bind at compile time
 let YamlFile = "/Users/andresv/Projects/river/river/config.yaml" // By using this provider I can read yaml
 
@@ -31,12 +34,30 @@ data.Save(YamlFile)
 
 
 (*  Twitter *)
-type TweetData = { message : string; image: string; publish_date: string; }
 
+type TweetData = {
+    message : string;
+    image: string;
+    publish_date: string;
+}
+
+// Auth
 let key = "w9HTFA5cCDPhmLqUYqrSK6kXK"
-let secret = "kkrn0e66g5hpKzLwEbnvY6KVR4pqUfDnJNBvoXcgjqCvZGu1Bv"
+let keySecret = "kkrn0e66g5hpKzLwEbnvY6KVR4pqUfDnJNBvoXcgjqCvZGu1Bv"
 let token = "55159989-NThkem3iDw1U1L3fEA0iLdwqYfeEkPHzLRfNPg4uI"
 let tokenSecret = "DboMLBHwRBVW2QQmpEXPBbs1UEMIeFkHNvEgxJT4q8Z8Z"
+
+Tweetinvi.Auth.SetUserCredentials(key, keySecret, token, tokenSecret) |> ignore
+
+// Image upload
+let image = File.ReadAllBytes("/Users/andresv/Projects/river/river/Test/primer-corruption.png")
+let imageMedia = Upload.UploadImage(image)
+
+// The tweet
+let parameters = new Parameters.PublishTweetOptionalParameters()
+parameters.Medias.Add(imageMedia)
+
+let tweet = Tweet.PublishTweet("This tweet is another test", parameters)
 
 
 (*  MAIN *)
