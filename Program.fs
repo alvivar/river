1ae50f9f3144f71cc9e0177110e77721635f2b03
alvivar <andresalvivar@gmail.com>
2017-02-18 20:53:44 -0600

    Chiron has been added to the project :D I'm playing with it

--- Program.fs
+++ Program.fs
@@ -26,36 +26,49 @@
         - Post once to twitter withing the time scheduled *)
 
 
-open Tweetinvi
 open System
 open System.IO
+open Tweetinvi
+open Chiron
 
 
 (*  Twitter *)
 
-type TweetData = { message : string; image: string; publish_date: string }
+// type TweetData = { message : string; image: string; publish_date: string; }
 
-// Auth
-let key = "w9HTFA5cCDPhmLqUYqrSK6kXK"
-let keySecret = "kkrn0e66g5hpKzLwEbnvY6KVR4pqUfDnJNBvoXcgjqCvZGu1Bv"
-let token = "55159989-NThkem3iDw1U1L3fEA0iLdwqYfeEkPHzLRfNPg4uI"
-let tokenSecret = "DboMLBHwRBVW2QQmpEXPBbs1UEMIeFkHNvEgxJT4q8Z8Z"
-Tweetinvi.Auth.SetUserCredentials(key, keySecret, token, tokenSecret) |> ignore
+// // Auth
+// let key = "w9HTFA5cCDPhmLqUYqrSK6kXK"
+// let keySecret = "kkrn0e66g5hpKzLwEbnvY6KVR4pqUfDnJNBvoXcgjqCvZGu1Bv"
+// let token = "55159989-NThkem3iDw1U1L3fEA0iLdwqYfeEkPHzLRfNPg4uI"
+// let tokenSecret = "DboMLBHwRBVW2QQmpEXPBbs1UEMIeFkHNvEgxJT4q8Z8Z"
+// Tweetinvi.Auth.SetUserCredentials(key, keySecret, token, tokenSecret) |> ignore
 
-// Image upload
-let image = File.ReadAllBytes((Directory.GetCurrentDirectory()) + "/test-images/primer-corruption.png")
-let imageMedia = Upload.UploadImage(image)
+// // Image upload
+// let image = File.ReadAllBytes((Directory.GetCurrentDirectory()) + "/example/primer_corruption.png")
+// let imageMedia = Upload.UploadImage(image)
 
-// The tweet
-let parameters = new Parameters.PublishTweetOptionalParameters()
-parameters.Medias.Add(imageMedia)
-let tweet = Tweet.PublishTweet("This tweet is a #test", parameters)
+// // The tweet
+// let parameters = new Parameters.PublishTweetOptionalParameters()
+// parameters.Medias.Add(imageMedia)
+// let tweet = Tweet.PublishTweet("This tweet is a #test", parameters)
 
 
 (*  MAIN *)
 [<EntryPoint>]
 let main argv =
-    printfn "Arguments %A" argv
+
+    printfn "Arguments -> %A" argv
     printfn "Current directory -> %s" (Directory.GetCurrentDirectory())
+
+    let formatExample =
+        Object <| Map.ofList [
+            "name", String "Marcus Griep"
+            "isAdmin", Bool true
+            "numbers", Array [ Number 1m; Number 2m; String "Fizz" ] ]
+
+    let formatCompact = Json.format formatExample
+    let formatPretty = Json.formatWith JsonFormattingOptions.Pretty formatExample
+
+    printf "%s" formatPretty
 
     0 // Exit code
