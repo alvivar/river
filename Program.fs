dfb2d1f80f0b5e0227ad841853690ef82789ab84
alvivar <andresalvivar@gmail.com>
2017-02-19 20:37:23 -0600

    I haven't managed to serialize the file list stuff, hm :/ But that's it for the day :)

--- Program.fs
+++ Program.fs
@@ -1,31 +1,35 @@
 ﻿
 (*  River v0.1
-    Queue tweets by dropping images into a folder!
+    A bot that tweets at the best times when you drop images into a folder!
 
     By twitter.com/matnesis
 
 
     Working on
-        x Learning to Yaml (doesn't work on netcore)
+        x Learning to Yaml (.Configuration doesn't work on netcore)
         x Learning to tweet images/text
         x Scan and list current folder for images
+        - Learn to JSON with Chiron
         - CRUD a config file based on images found
-        - Learn to JSON via Chiron
         - Chat though console to communicate with the bot
-            - Explain defaults and commands
-            - Set tweets per day
-            - Set auto schedule mode based on tweets per day
-            - Sort queue
-            - Set fixed schedule mode
-            - Exit
+            - help | explain defaults and commands
+            - scan <dir> | Analyzes a folder and create a config file with the defaults
+            - tweet daily <#> | Set tweets per day
+            - schedule su mo tu we th fr sa 7a 9: 7p 9:3 | Sets the manual schedule
+            - sort 3 2 5, Sorts the queue
+            - enable | play | activate
+            - disable | stop | deactivate
+            - exit | quit | die
+            - schedule auto | Set auto schedule mode using tweets per day
         - Read from a config file with time to post
-        - Post once to twitter withing the time scheduled
+        - Waita and tweet withing the schedule
         - Best time to tweet
             - Followers, following analysis
             - Interactions analysis
             - Prediction model *)
 
 
+open FSharp.Core
 open System
 open System.IO
 
@@ -56,11 +60,11 @@ open Chiron
 
 (* CHIRON *)
 
-// let formatExample =
-//         Object <| Map.ofList [
-//             "name", String "Marcus Griep"
-//             "isAdmin", Bool true
-//             "numbers", Array [ Number 1m; Number 2m; String "Fizz" ] ]
+let formatExample =
+        Object <| Map.ofList [
+            "name", String "Marcus Griep"
+            "isAdmin", Bool true
+            "numbers", Array [ Number 1m; Number 2m; String "Fizz" ] ]
 
 // let formatCompact = Json.format formatExample
 // let formatPretty = Json.formatWith JsonFormattingOptions.Pretty formatExample
@@ -69,14 +73,23 @@ open Chiron
 // printfn "%s\n" formatPretty
 
 
+type Files =
+    { files : string array }
+    static member ToJson (x : Files) = json {
+        do! Json.write "files" x.files
+    }
+    static member FromJson (_ : Files) = json {
+        let! fs = Json.read "files"
+        return { files = fs }
+    }
 
-// Returns all files and folders.
+
+// Returns a sequence with all files and folders.
 let rec allFiles dirs =
     match dirs with
     | dirs when Seq.isEmpty dirs -> Seq.empty
     | _ -> seq { yield! dirs |> Seq.collect Directory.EnumerateFiles
                  yield! dirs |> Seq.collect Directory.EnumerateDirectories |> allFiles }
-
 
 
 (*  MAIN *)
@@ -91,7 +104,22 @@ let main argv =
 
 
     // Print all files and folders
-    [| dir |] |> allFiles |> Seq.iter (fun x -> printfn "> %s" x)
+    // [| dir |]
+    //     |> allFiles
+    //     |> Seq.iter (fun x -> printfn "> %s" x)
+
+    let jfiles =
+        { files = [| dir |] |> allFiles |> Array.ofSeq }
+        |> Json.serialize
+        |> Json.format
+
+    // let flist = [| dir |] |> allFiles |> Array.ofSeq |> Json.m
+
+    // let formatExample =
+    //     Object <| Map.ofList [
+    //         "files", flist ]
+
+    // printfn "%s" jfiles
 
 
     0 // Exit code
