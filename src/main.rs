9d388d6e755237bf14fdf5d1563383bd1789bb5b
Andrés Villalobos <andresalvivar@gmail.com>
2021-03-10 22:18:09 -0600

    A prototype that gets the auth_url for the application with egg_mode.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    token().await;
+}
+
+async fn token() {
+    let con_token = egg_mode::KeyPair::new(
+        "gCOpE7s0QP1rfnbNdS7DNEfF1",
+        "FYYE8BzAKjNmo51fZjrTMio1Z1pAF67oPVfG07tMQSJHeQmbSo",
+    );
+
+    // "oob" is needed for PIN-based auth; see docs for `request_token` for more info
+    let request_token = egg_mode::auth::request_token(&con_token, "oob")
+        .await
+        .unwrap();
+
+    let auth_url = egg_mode::auth::authorize_url(&request_token);
+    println!("{}", auth_url);
 }
