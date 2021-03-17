2a630f959d76540b96a648c8b8e7305460d294d9
Andrés Villalobos <andresalvivar@gmail.com>
2021-03-17 02:13:21 -0600

    River can append new found images, and return itself as Text for serialization.

--- src/river.rs
+++ src/river.rs
@@ -1,8 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::Path};
 
 pub const TWEET: &str = "tweet";
 pub const IMAGE: &str = "image";
 pub const STATE: &str = "state";
+
+pub const PENDING: &str = "pending";
+pub const SENT: &str = "sent";
+pub const ERROR: &str = "error";
 
 #[derive(Debug)]
 pub struct Days {
@@ -39,9 +43,9 @@ impl Days {
 
 #[derive(Debug)]
 pub struct Tweet {
-    text: String,
-    image: String,
-    state: String,
+    pub text: String,
+    pub image: String,
+    pub state: String,
 }
 
 impl Tweet {
@@ -56,7 +60,7 @@ impl Tweet {
 
 #[derive(Debug)]
 pub struct Tweets {
-    queue: Vec<Tweet>,
+    pub queue: Vec<Tweet>,
 }
 
 impl Tweets {
@@ -80,7 +84,7 @@ impl River {
         River { days, tweets }
     }
 
-    pub fn parse(&mut self, content: String) {
+    pub fn parse_load(&mut self, content: String) {
         // Two lines defines our sections.
         for block in content.split("\n\n") {
             // One line is the key and the value.
@@ -156,21 +160,89 @@ impl River {
             }
         }
     }
-}
 
-fn append_new(tweets: &mut Vec<Tweet>, image: String) {
-    match tweets.iter_mut().find(|ref p| image <= p.image) {
-        Some(pivot) => {
-            // We don't need to update the one we already have.
-            return;
-        }
-        // o/w insert a new leaf at the end
-        None => {
-            let mut tweet = Tweet::new();
-            tweet.image = image;
+    pub fn append_new(&mut self, image: String) {
+        match self
+            .tweets
+            .queue
+            .iter_mut()
+            .find(|ref p| image.trim() == p.image.trim())
+        {
+            // This place is useful to update because it exists.
+            Some(_) => {}
 
-            tweets.push(tweet);
+            // New entry!
+            None => {
+                let mut tweet = Tweet::new();
+                tweet.image = image;
+
+                self.tweets.queue.push(tweet);
+            }
         }
+    }
+
+    pub fn to_content(&self, with_name_as_text: bool) -> String {
+        let mut content = String::new();
+
+        content.push_str("schedule]\n\n");
+
+        content.push_str("# Custom times and tags per day, as much as you like.\n\n");
+
+        // @todo Schedule from the file or default.
+
+        content.push_str("mon]  8a   10a  #monday\n");
+        content.push_str("tue]  9a   11a  #tuesday\n");
+        content.push_str("wed]  10a  12p  #wednesday\n");
+        content.push_str("thu]  11a  1p   #thursday\n");
+        content.push_str("fri]  12p  2p   #friday\n");
+        content.push_str("sat]  1p   3p   #saturday\n");
+        content.push_str("sun]  2p   4p   #sunday\n\n\n");
+
+        content.push_str("tweets]\n\n");
+
+        content.push_str("# tweet] Message to tweet! #cool\n");
+        content.push_str("# image] Image-to.tweet\n");
+        content.push_str("# state] Pending | Sent | Error <- Handled by the application.\n\n");
+
+        content.push_str("# All fields are optional.\n");
+        content.push_str("# If you want you can send a single tweet] or a single image].\n\n");
+
+        for file in &self.tweets.queue {
+            let mut text = file.text.to_owned();
+            let image = &file.image;
+
+            let state = if file.state.len() > 0 {
+                let state = match file.state.to_lowercase().as_str() {
+                    PENDING => "Pending",
+                    SENT => "Sent",
+                    ERROR => "Error",
+                    _ => PENDING,
+                };
+
+                state.to_string()
+            } else {
+                PENDING.to_owned()
+            };
+
+            if with_name_as_text && text.len() < 1 {
+                let image_path = Path::new(&image);
+                let name = image_path
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string();
+
+                text = name.replace('-', " ").replace('_', " ").replace('.', " ");
+            }
+
+            content.push_str(&format!("{}] {}\n", TWEET, text,));
+            content.push_str(&format!("{}] {}\n", IMAGE, image));
+            content.push_str(&format!("{}] {}\n", STATE, state));
+            content.push_str(&format!("\n"));
+        }
+
+        content
     }
 }
 
@@ -178,3 +250,7 @@ fn remove_whitespace(s: &str) -> String {
     let sc: Vec<&str> = s.split_whitespace().collect();
     sc.join(" ")
 }
+
+// fn remove_whitespace_inplace(s: &mut String) {
+//     s.retain(|c| !c.is_whitespace());
+// }
