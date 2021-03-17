e3fbef306e3c644a58166198d57c442974dd46be
Andrés Villalobos <andresalvivar@gmail.com>
2021-03-16 22:52:22 -0600

    River extracted.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod egg;
 
+mod river;
+use river::River;
+
 use std::{
-    collections::HashMap,
     env,
     io::{Read, Write},
 };
@@ -11,76 +13,10 @@ use self_update::cargo_crate_version;
 use walkdir::{DirEntry, WalkDir};
 
 const QUEUE_FILE: &str = "river.queue.txt";
-const TWEET: &str = "tweet";
-const IMAGE: &str = "image";
-const STATE: &str = "state";
 
 const PENDING: &str = "Pending";
 // const SENT: &str = "Send";
 // const ERROR: &str = "Error";
-
-#[derive(Debug)]
-struct Tweet {
-    text: String,
-    image: String,
-    state: String,
-}
-
-impl Tweet {
-    pub fn new() -> Tweet {
-        let text = String::new();
-        let image = String::new();
-        let state = String::new();
-
-        Tweet { text, image, state }
-    }
-}
-
-#[derive(Debug)]
-struct Tweets {
-    queue: Vec<Tweet>,
-}
-
-impl Tweets {
-    pub fn new() -> Tweets {
-        let queue = Vec::new();
-
-        Tweets { queue }
-    }
-}
-
-#[derive(Debug)]
-struct Days {
-    mon: Vec<String>,
-    tue: Vec<String>,
-    wed: Vec<String>,
-    thu: Vec<String>,
-    fri: Vec<String>,
-    sat: Vec<String>,
-    sun: Vec<String>,
-}
-
-impl Days {
-    pub fn new() -> Days {
-        let mon = Vec::<String>::new();
-        let tue = Vec::<String>::new();
-        let wed = Vec::<String>::new();
-        let thu = Vec::<String>::new();
-        let fri = Vec::<String>::new();
-        let sat = Vec::<String>::new();
-        let sun = Vec::<String>::new();
-
-        Days {
-            mon,
-            tue,
-            wed,
-            thu,
-            fri,
-            sat,
-            sun,
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -104,21 +40,33 @@ async fn main() {
         )
         .get_matches();
 
-    // Clap
-
-    // Start waiting and posting accordinly to the queue file on the folder.
-
-    // Create the river file by scanning image files on the folder.
+    // Create or @todo updates the river file by scanning image files on the folder.
     if let Some(matches) = matches.subcommand_matches("scan") {
         let has_name = matches.is_present("name");
 
+        // Parse the river file, if exists.
+        let mut content = String::new();
+        if let Ok(mut file) = std::fs::File::open(QUEUE_FILE) {
+            file.read_to_string(&mut content).unwrap();
+        }
+
+        let mut river = River::new();
+        river.parse(content);
+
+        // Look for new files.
         let current_dir = env::current_dir().unwrap();
 
         let mut files: Vec<DirEntry> = Vec::new();
         for entry in WalkDir::new(current_dir) {
+            // @todo Check for images only.
             files.push(entry.unwrap());
         }
 
+        // @todo Update River with the new files.
+
+        // @todo Create the River file.
+
+        // Content
         let mut content = String::new();
 
         content.push_str("schedule]\n\n");
@@ -151,9 +99,9 @@ async fn main() {
                 desc = "".to_owned();
             }
 
-            content.push_str(&format!("{}] {}\n", TWEET, desc,));
-            content.push_str(&format!("{}] {:?}\n", IMAGE, path));
-            content.push_str(&format!("{}] {}\n", STATE, PENDING));
+            content.push_str(&format!("{}] {}\n", river::TWEET, desc,));
+            content.push_str(&format!("{}] {:?}\n", river::IMAGE, path));
+            content.push_str(&format!("{}] {}\n", river::STATE, PENDING));
             content.push_str(&format!("\n"));
         }
 
@@ -161,100 +109,24 @@ async fn main() {
         file.write_all(content.trim().as_bytes()).unwrap();
     }
 
-    // Using the
+    // Using the river file,
     if let Some(_) = matches.subcommand_matches("start") {
         // Twitter authentication.
         egg::Config::load().await;
 
-        // Updates the config file with new files found.
-        let mut config = String::new();
-        let mut tweets = Tweets::new();
-        let mut days = Days::new();
-
+        // Parse the river file.
+        let mut content = String::new();
         if let Ok(mut file) = std::fs::File::open(QUEUE_FILE) {
-            file.read_to_string(&mut config).unwrap();
-
-            // Two lines defines our sections.
-            for block in config.split("\n\n") {
-                // One line is the key and the value.
-                for line in block.split('\n') {
-                    if !line.contains(']') {
-                        continue;
-                    }
-
-                    let mut kv = line.split(']');
-                    let k = kv.next().unwrap().trim();
-                    let v = kv.next().unwrap().trim();
-
-                    let v = remove_whitespace(v);
-
-                    // It's a day?
-                    match k.to_lowercase().as_str() {
-                        "mon" => days.mon.push(v),
-                        "tue" => days.tue.push(v),
-                        "wed" => days.wed.push(v),
-                        "thu" => days.thu.push(v),
-                        "fri" => days.fri.push(v),
-                        "sat" => days.sat.push(v),
-                        "sun" => days.sun.push(v),
-
-                        TWEET => {
-                            // Tweeting in his own parsing dimension for the complete block.
-                            let mut tweet = Tweet::new();
-
-                            let mut key_value: HashMap<String, Vec<String>> = HashMap::new();
-
-                            let mut last_key = TWEET.to_owned();
-
-                            for line in block.split('\n') {
-                                if !line.contains(']') {
-                                    key_value
-                                        .entry(last_key.to_owned())
-                                        .or_insert_with(Vec::new)
-                                        .push(line.to_owned());
-
-                                    continue;
-                                }
-
-                                let mut kv = line.split(']');
-                                let k = kv.next().unwrap().trim();
-                                let v = kv.next().unwrap().trim();
-
-                                last_key = k.to_owned();
-
-                                key_value
-                                    .entry(k.to_owned())
-                                    .or_insert_with(Vec::new)
-                                    .push(v.to_owned());
-                            }
-
-                            let tweet_val =
-                                key_value.entry(TWEET.to_owned()).or_insert_with(Vec::new);
-                            let text = tweet_val.join("");
-
-                            let image_val =
-                                key_value.entry(IMAGE.to_owned()).or_insert_with(Vec::new);
-                            let image = image_val.join("");
-
-                            let state_val =
-                                key_value.entry(STATE.to_owned()).or_insert_with(Vec::new);
-                            let state = state_val.join("");
-
-                            tweet.text = text;
-                            tweet.image = image;
-                            tweet.state = state;
-
-                            tweets.queue.push(tweet);
-                        }
-
-                        _ => continue,
-                    }
-                }
-            }
-
-            println!("{:?}\n", tweets);
-            println!("{:?}\n", days);
+            file.read_to_string(&mut content).unwrap();
         }
+
+        let mut river = River::new();
+        river.parse(content);
+
+        println!("{:?}\n", river.tweets);
+        println!("{:?}\n", river.days);
+
+        // @todo Send the river file to the Tweeting thread.
     }
 
     // Self updates.
@@ -268,11 +140,6 @@ async fn main() {
     }
 
     println!("Done!");
-}
-
-fn remove_whitespace(s: &str) -> String {
-    let sc: Vec<&str> = s.split_whitespace().collect();
-    sc.join(" ")
 }
 
 // fn remove_whitespace_inplace(s: &mut String) {
