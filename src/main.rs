832280cc44c7fd7c590318d8add6686eb5b7156b
Andrés Villalobos <andresalvivar@gmail.com>
2021-03-24 00:00:52 -0600

    Polished +1 Shinning +1

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-mod egg;
-
-mod river;
-use river::River;
-
-use egg_mode::{
-    media::{media_types, upload_media},
-    tweet::DraftTweet,
-};
-
 use std::{
     env,
     io::{Read, Write},
     path::PathBuf,
-    thread, u32, u64,
+    process::exit,
+    u32, u64,
+};
+
+use tokio;
+use tokio::time;
+
+use egg_mode::{
+    media::{media_types, upload_media},
+    tweet::DraftTweet,
 };
 
 use chrono::{Datelike, Duration, Local, Timelike};
@@ -20,34 +19,36 @@ use clap::{App, AppSettings::ArgRequiredElseHelp, Arg, SubCommand};
 use self_update::cargo_crate_version;
 use walkdir::WalkDir;
 
+mod egg;
+mod river;
+use river::River;
+
 const QUEUE_FILE: &str = "river.queue.txt";
 
 #[tokio::main]
 async fn main() {
-    // CLAP
-
     let matches = App::new("river")
         .version(cargo_crate_version!())
-        .about("Check out github.com/alvivar/river for more info!")
+        .about("Tool that schedules tweets with images\ngithub.com/alvivar/river")
         .setting(ArgRequiredElseHelp)
         .subcommand(SubCommand::with_name("scan")
-            .about("Scans the current dir and creates (or updates) the 'river.queue.txt' file with all images found, ready to be tweeted")
+            .about("Scans the current dir and creates (or updates) river.queue.txt with all images found")
             .arg(Arg::with_name("name")
                 .short("n")
-                .help("Use the file name as text in the river file")
+                .help("Use the name as text")
             )
         )
         .subcommand(SubCommand::with_name("start")
-            .about("Starts tweeting from the 'river.queue.txt' file")
+            .about("Starts tweeting from river.queue.txt")
         )
         .subcommand(SubCommand::with_name("update")
-            .about("Self updates to the latest release on GitHub")
+            .about("Self updates from the latest github release")
         )
         .get_matches();
 
-    // SCAN
+    // Scan
 
-    // Create or updates the river file by scanning image files on the folder.
+    // Create or updates the River file by scanning image files on the folder.
     if let Some(matches) = matches.subcommand_matches("scan") {
         let name_as_text = matches.is_present("name");
 
@@ -88,16 +89,13 @@ async fn main() {
         println!("File {} generated\n", QUEUE_FILE);
     }
 
-    // START
+    // Start
 
-    // Using the river file, lets start waiting to tweet.
+    // Using the River file, lets start waiting to tweet.
     if let Some(_) = matches.subcommand_matches("start") {
-        // Twitter authentication.
+        let auth = egg::Config::load().await;
 
-        let config = egg::Config::load().await;
-
-        // Parse the river file.
-
+        // Parse the River file.
         let mut content = String::new();
         if let Ok(mut file) = std::fs::File::open(QUEUE_FILE) {
             file.read_to_string(&mut content).unwrap();
@@ -107,23 +105,18 @@ async fn main() {
         river.parse_load(content);
 
         // Tweet at the next hour.
-
-        // @todo There is probably a better way to do this.
-        let mut queue = river.tweets.queue.clone();
-
+        let mut queue = river.tweets.queue.clone(); // @todo There is probably a better way to do this.
         for tweet in &mut queue {
-            // Skip already tweeted tweets.
-            if tweet.state.to_lowercase().trim() != river::PENDING {
+            if tweet.state.trim().to_lowercase() != river::PENDING {
                 continue;
             }
 
-            // For the current day, which is the closest hour?
+            // On the current day, which is the closest hour?
             let local = Local::now();
             let today = local.weekday();
-
             let hour = local.hour();
-            let hours = river.get_day(&today.to_string().as_str());
 
+            let hours = river.get_day(&today.to_string().as_str());
             let mut schedule: Vec<u32> = hours
                 .split(" ")
                 .filter_map(|x| match x.parse::<u32>() {
@@ -137,26 +130,27 @@ async fn main() {
             schedule.dedup();
 
             let count = schedule.iter().len();
-            let mut schedule_today = schedule.iter();
+            let mut today_schedule = schedule.iter();
 
             // Some info.
-
             println!("{}, {} hour", today, hour);
 
             // Is there a next hour?
-            match schedule_today.next() {
+            match today_schedule.next() {
                 Some(hour) => {
                     let mut hour = *hour;
                     let mut minute = 0;
+                    let mut secs = 0;
 
                     if hour > 23 {
                         hour = 23;
                         minute = 59;
+                        secs = 59;
                     }
 
-                    // Time until the next hour.
+                    // Time to the next hour.
                     let now = Local::now();
-                    let next_hour = now.date().and_hms(hour, minute, 0);
+                    let next_hour = now.date().and_hms(hour, minute, secs);
                     let duration = next_hour.signed_duration_since(now).to_std();
 
                     // Tweeting or waiting.
@@ -165,17 +159,19 @@ async fn main() {
                             let secs = duration.as_secs() + 1;
                             let mins = secs / 60;
 
-                            println!(" Tweeting at {}:00", hour);
+                            println!(" Next tweet at {}:00", hour);
                             println!(" {} pending today", count);
-                            println!(" Waiting {} minutes until the next tweet", mins);
+                            println!(" Waiting {} minutes to tweet", mins);
 
-                            thread::sleep(std::time::Duration::from_secs(secs));
+                            time::delay_for(time::Duration::from_secs(secs)).await
                         }
+                        // The OutOfRangeError means that duration is less than 1 hour.
                         Err(_) => {
-                            let image_path = PathBuf::from(tweet.image.to_owned());
-                            let mut post = DraftTweet::new(tweet.text.to_owned());
+                            let text = tweet.text.to_owned();
 
-                            let media_type = match image_path
+                            let image = PathBuf::from(tweet.image.to_owned());
+
+                            let media_type = match image
                                 .as_path()
                                 .extension()
                                 .and_then(|os| os.to_str())
@@ -187,27 +183,34 @@ async fn main() {
                                 "webp" => media_types::image_webp(),
                                 "mp4" => media_types::video_mp4(),
                                 _ => {
-                                    eprintln!(" Format not recognized, must be one of jpg, jpeg, gif, png, webp, mp4");
-                                    std::process::exit(1);
+                                    eprintln!(" Image format not recognized, must be one of [jpg, jpeg, gif, png, webp, mp4]");
+                                    exit(1);
                                 }
                             };
 
-                            let bytes = std::fs::read(image_path).unwrap();
-                            let handle = upload_media(&bytes, &media_type, &config.token)
-                                .await
-                                .unwrap();
+                            let mut post = DraftTweet::new(text.to_owned());
+
+                            let data = std::fs::read(image.to_owned()).unwrap();
+                            let handle =
+                                upload_media(&data, &media_type, &auth.token).await.unwrap();
 
                             post.add_media(handle.id.clone());
 
-                            let sent = post.send(&config.token).await;
+                            println!("  Trying...");
+                            println!("  > {}", text);
+                            println!("  > {:?}", image);
 
-                            match sent {
+                            match post.send(&auth.token).await {
                                 Ok(_) => {
                                     tweet.state = river::SENT.to_owned();
 
-                                    println!(" Sent!")
+                                    println!(" Sent!");
                                 }
-                                Err(_) => println!(" Tweet error!"),
+                                Err(_) => {
+                                    println!(" Tweet error!");
+
+                                    exit(1);
+                                }
                             }
 
                             let now = Local::now();
@@ -216,8 +219,7 @@ async fn main() {
 
                             println!(" Waiting {} minutes until the next hour", mins);
 
-                            thread::sleep(std::time::Duration::from_secs(5));
-                            // thread::sleep(std::time::Duration::from_secs(secs));
+                            time::delay_for(time::Duration::from_secs(secs)).await
                         }
                     }
                 }
@@ -234,30 +236,29 @@ async fn main() {
                     println!(" No tweets today");
                     println!(" Waiting {} hours until tomorrow", hours);
 
-                    thread::sleep(std::time::Duration::from_secs(secs));
+                    time::delay_for(time::Duration::from_secs(secs)).await
                 }
             }
 
             println!();
         }
 
-        // Update the current file.
-
+        // Update the River file.
         river.tweets.queue = queue;
 
         let content = river.to_text(false);
         write_file(content, QUEUE_FILE);
     }
 
-    // UPDATE
+    // Update
 
     // Self updates.
     if let Some(_matches) = matches.subcommand_matches("update") {
         println!();
 
         match update() {
-            Ok(_) => {}
-            Err(_) => println!("Error updating."),
+            Ok(_) => println!("Updated!"),
+            Err(_) => println!("Error updating!"),
         }
     }
 
